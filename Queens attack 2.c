fb13897d42e6c5b9ffbb3ad084151a7f8591751b
wintersoldie <73120282+wintersoldie@users.noreply.github.com>
2020-10-19 19:46:30 +0530

    Add files via upload

---
+++ Queens attack 2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main (){
+    int n,l=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<100;i++)
+    for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+
+}
